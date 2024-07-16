bf0b9e4fec72ccc470057331983645079c35ed71
pushpendrasingh <pushpendra.singh@likeminds.community>
2024-07-16 18:20:29 +0530

    [LM-12513] Added create poll class

---
+++ LMChatCore_iOS/LMChatCore_iOS/Source/Module/Poll/CreatePoll/LMChatCreatePollViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LMChatCreatePollViewController.swift
+//  LikeMindsChatCore
+//
+//  Created by Pushpendra Singh on 16/07/24.
+//
+
+import Foundation
+import LikeMindsChatUI
+
+open class LMChatCreatePollViewController: LMViewController {
+    
+    var viewModel: LMChatCreatePollViewModel?
+    
+}
---
+++ LMChatCore_iOS/LMChatCore_iOS/Source/Module/Poll/CreatePoll/LMChatCreatePollViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LMChatCreatePollViewModel.swift
+//  LikeMindsChatCore
+//
+//  Created by Pushpendra Singh on 16/07/24.
+//
+
+import Foundation
+import LikeMindsChat
+import LikeMindsChatUI
+
+public protocol LMChatCreatePollViewModelProtocol: AnyObject {
+ 
+}
+
+public class LMChatCreatePollViewModel: LMChatBaseViewModel {
+    
+}
